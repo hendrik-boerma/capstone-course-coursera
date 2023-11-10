2884522d49421c4702dc1f2a75b61ad160bd23f5
Hendrik Boerma <hendrikboerma@gmail.com>
2023-11-10 12:37:33 +0100

    routing

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,21 +1,23 @@
-import { Routes, Route  } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Menu from '../pages/Menu';
 import BookingPage from '../pages/BookingPage';
 import BookingConfirmation from "../pages/BookingConfirmation";
 
-function Routing () {
-
-    return (
-        <Routes basename="/capstone-course-coursera/">
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/capstone-course-coursera/about" element={<About />}></Route>
-        <Route path="/capstone-course-coursera/menu" element={<Menu />}></Route>
-        <Route path="/capstone-course-coursera/booking" element={<BookingPage/>}></Route>
-        <Route path="/capstone-course-coursera/confirmation" element={<BookingConfirmation />}></Route>
+function Routing() {
+  return (
+    <Router basename="/capstone-course-coursera">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/confirmation" element={<BookingConfirmation />} />
       </Routes>
-    );
+    </Router>
+  );
 }
 
 export default Routing;
